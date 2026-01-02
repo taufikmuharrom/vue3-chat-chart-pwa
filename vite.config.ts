88a2164e65e3ae83913e9455bd2740708a3b1ee1
taufikmuharrom <taufikmuharromdev@gmail.com>
2026-01-02 14:30:32 +0700

    refactor : update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue3-chat-chart-pwa/',
   plugins: [
     vue(),
     vueDevTools(),
