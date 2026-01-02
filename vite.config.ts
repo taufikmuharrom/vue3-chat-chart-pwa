79ad4e7b5ae71ebeeb20c4d14587e76710a52075
taufikmuharrom <taufikmuharromdev@gmail.com>
2026-01-02 13:44:02 +0700

    feat : complete pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -21,16 +21,27 @@ export default defineConfig({
         theme_color: '#FF6B00',
         background_color: '#FFFFFF',
         display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        orientation: 'any',
         icons: [
           {
             src: '/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any',
           },
           {
             src: '/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
