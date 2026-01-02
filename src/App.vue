df879a42cff815c241f2c1cae89d2a17dd56ca5b
taufikmuharrom <taufikmuharromdev@gmail.com>
2026-01-02 08:33:29 +0700

    setup project and install lib

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="bg-red-200 p-4 rounded-2xl">HELLO WORLD</div>
+    <Button>Button</Button>
+  </div>
+</template>
